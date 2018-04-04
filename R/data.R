9c712c868cb34985d18518db682b8e32b32c6af5
Wanding Zhou <wanding.zhou@vai.org>
2018-04-04 10:40:37 -0400

    modified data upload
    Former-commit-id: d38c62296f527a1f5cee857045e8048c74753d5c
    Former-commit-id: 669b5c997e17aaf2dae9387591fcb0ba4c2d779b
    Former-commit-id: df1ad9f37b92168dde50d4d254667e419620840e

---
+++ R/data.R
@@ -0,0 +1,186 @@
+#' HM27 probe ordering
+#'
+#' HM27 probe chip address
+"HM27.ordering"
+
+#' HM450 probe ordering
+#'
+#' HM450 probe chip address
+"HM450.ordering"
+
+#' EPIC probe ordering
+#'
+#' EPIC probe chip address
+"EPIC.ordering"
+
+#' Cytoband information of hg19
+"cytoBand.hg19"
+
+#' Cytoband information of hg38
+"cytoBand.hg38"
+
+#' EPIC.controls
+"EPIC.controls"
+
+#' EPIC.female.clean.chrY.probes
+"EPIC.female.clean.chrY.probes"
+
+#' EPIC.female.xlinked.chrX.probes
+"EPIC.female.xlinked.chrX.probes"
+
+#' EPIC.hg19.probe2chr
+"EPIC.hg19.probe2chr"
+
+#' EPIC.mapped.probes.hg19
+"EPIC.mapped.probes.hg19"
+
+#' EPIC.mapped.probes.hg38
+"EPIC.mapped.probes.hg38"
+
+#' EPIC.mask
+"EPIC.mask"
+
+#' EPIC.typeI.extC
+"EPIC.typeI.extC"
+
+#' EPIC.typeI.extT
+"EPIC.typeI.extT"
+
+#' ethnicity.ccs.probes
+"ethnicity.ccs.probes"
+
+#' ethnicity.model
+"ethnicity.model"
+
+#' ethnicity.rs.probes
+"ethnicity.rs.probes"
+
+#' hg19.chrominfo
+"hg19.chrominfo"
+
+#' hg38.chrominfo
+"hg38.chrominfo"
+
+#' M27.betas.leuko.whole
+"HM27.betas.leuko.whole"
+
+#' HM27.controls
+"HM27.controls"
+
+#' HM27.hg19.probe2chr
+"HM27.hg19.probe2chr"
+
+#' HM27.mask
+"HM27.mask"
+
+#' HM450.betas.leuko.whole
+"HM450.betas.leuko.whole"
+
+#' HM450.controls
+"HM450.controls"
+
+#' M450.female.clean.chrY.probes
+"HM450.female.clean.chrY.probes"
+
+#' HM450.female.xlinked.chrX.probes
+"HM450.female.xlinked.chrX.probes"
+
+#' HM450.hg19.probe2chr
+"HM450.hg19.probe2chr"
+
+#' HM450.mapped.probes.hg19
+"HM450.mapped.probes.hg19"
+
+#' HM450.mapped.probes.hg38
+"HM450.mapped.probes.hg38"
+
+#' HM450.mask
+"HM450.mask"
+
+#' HM450.mask.tcga
+"HM450.mask.tcga"
+
+#' HM450.typeI.extC
+"HM450.typeI.extC"
+
+#' HM450.typeI.extT
+"HM450.typeI.extT"
+
+#' sex.inference.model
+"sex.inference.model"
+
+#' UCSC.refGene.gene2txn.hg19
+"UCSC.refGene.gene2txn.hg19"
+
+#' UCSC.refGene.gene2txn.hg38
+"UCSC.refGene.gene2txn.hg38"
+
+#' UCSC.refGene.txn2gene.hg19
+"UCSC.refGene.txn2gene.hg19"
+
+#' UCSC.refGene.txn2gene.hg38
+"UCSC.refGene.txn2gene.hg38"
+
+#' CSC.refGene.txns.hg19
+"UCSC.refGene.txns.hg19"
+
+#' UCSC.refGene.txns.hg38
+"UCSC.refGene.txns.hg38"
+
+#' EPIC.betas.leuko.whole
+"EPIC.betas.leuko.whole"
+
+#' 353 age predictor probes from Horvath 2013
+"agePredHorvath353"
+
+#' cellref.buccal
+"cellref.buccal"
+
+#' cellref.CD14Monocytes
+"cellref.CD14Monocytes"
+
+#' cellref.CD19B
+"cellref.CD19B"
+
+#' cellref.CD4T
+"cellref.CD4T"
+
+#' cellref.CD56NK
+"cellref.CD56NK"
+
+#' cellref.CD8T
+"cellref.CD8T"
+
+#' cellref.eosinophil
+"cellref.eosinophil"
+
+#' cellref.granulocytes
+"cellref.granulocytes"
+
+#' cellref.hair
+"cellref.hair"
+
+#' cellref.liver
+"cellref.liver"
+
+#' cellref.muscle
+"cellref.muscle"
+
+#' cellref.neutrophil
+"cellref.neutrophil"
+
+#' cellref.omentum
+"cellref.omentum"
+
+#' cellref.saliva
+"cellref.saliva"
+
+#' cellref.scFat
+"cellref.scFat"
+
+#' cellref.skin
+"cellref.skin"
+
+#' cellref.spleen
+"cellref.spleen"
+
