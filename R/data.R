8ee28b2b37018365fe0f14bfc667ef61601e9f56
Wanding Zhou <zhouwanding@gmail.com>
2018-05-25 11:40:38 +0800

    revert BiocParallel ipclock()
    Former-commit-id: 65903480f6ee8cfbaffb5e0315a1964ae2988ddd

--- R/data.R
+++ R/data.R
@@ -1,15 +1,12 @@
 
 cacheEnv <- new.env()
-id <- BiocParallel::ipcid()
 
 .sesameDataGet <- function(title) {
     if (!exists(title, envir=cacheEnv, inherits=FALSE)) {
-        BiocParallel::ipclock(id)
         eh <- query(ExperimentHub(localHub=TRUE), 'sesameData')
         if (title %in% eh$title) {
             assign(title, eh[[which(eh$title == title)]], envir=cacheEnv)
         }
-        BiocParallel::ipcunlock(id)
     }
     return(get(title, envir=cacheEnv, inherits=FALSE))
 }
@@ -22,7 +19,7 @@ id <- BiocParallel::ipcid()
 #' @import ExperimentHub
 #' @import AnnotationHub
 #' @examples
-#'
+#' 
 #' result <- sesameDataGet('genomeInfo.hg38')
 #' @export
 sesameDataGet <- function(title, verbose=FALSE) {
